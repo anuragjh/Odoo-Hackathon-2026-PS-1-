42316f142d9d87fa32508576ac7d5a5c92ab9975
SangikGhosh <sangik.ghosh1@gmail.com>
2026-07-12 10:03:04 +0530

    Add java-main-backend service

---
+++ services/java-microservice/java-main-backend/src/main/java/com/java/javamainbackend/JavaMainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.java.javamainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMainBackendApplication.class, args);
+    }
+
+}
---
+++ services/java-microservice/java-main-backend/src/test/java/com/java/javamainbackend/JavaMainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.javamainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
